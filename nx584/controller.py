a68071dab494a9f50c771728595c7b8119537bdb
Dan Smith <dsmith@danplanet.com>
2015-12-27 22:12:55 -0800

    Move flag definitions to model.py

--- nx584/controller.py
+++ nx584/controller.py
@@ -185,28 +185,13 @@ class NXController(object):
         types = frame.data[2:5]
         zone.state = bool(condition & 0x01)
 
-        status_flags = [
-            'Faulted', 'Trouble', 'Bypass',
-            'Inhibit', 'Low battery', 'Loss of supervision',
-            'Reserved']
         zone.condition_flags = []
-        for index, string in enumerate(status_flags):
+        for index, string in enumerate(model.Zone.STATUS_FLAGS):
             if condition & (1 << index):
                 zone.condition_flags.append(string)
 
         zone.type_flags = []
-        type_flags = [
-            ['Fire', '24 hour', 'Key-switch', 'Follower',
-             'Entry / exit delay 1', 'Entry / exit delay 2',
-             'Interior', 'Local only'],
-            ['Keypad sounder', 'Yelping Siren', 'Steady Siren',
-             'Chime', 'Bypassable', 'Group bypassable', 'Force armable',
-             'Entry guard'],
-            ['Fast loop response', 'Double EOL tamper', 'Trouble',
-             'Cross zone', 'Dialer delay', 'Swinger shutdown',
-             'Restorable', 'Listen in'],
-        ]
-        for byte, flags in enumerate(type_flags):
+        for byte, flags in enumerate(model.Zone.TYPE_FLAGS):
             for bit, name in enumerate(flags):
                 if types[byte] & (1 << bit):
                     zone.type_flags.append(name)
@@ -221,33 +206,10 @@ class NXController(object):
             ext.obj.zone_status(zone)
 
     def process_msg_6(self, frame):
-        condition_flags = [
-            ['Bypass code required', 'Fire trouble', 'Fire',
-             'Pulsing buzzer', 'TLM fault memory', 'reserved',
-             'Armed', 'Instant'],
-            ['Previous alarm', 'Siren on', 'Steady siren on',
-             'Alarm memory', 'Tamper', 'Cancel command entered',
-             'Code entered', 'Cancel pending'],
-            ['Reserved', 'Silent exit enabled', 'Entryguard (stay mode)',
-             'Chime mode on', 'Entry', 'Delay expiration warning',
-             'Exit 1', 'Exit 2'],
-            ['LED extinguish', 'Cross timing', 'Recent closing being timed',
-             'Reserved', 'Exit error triggered', 'Auto home inhibited',
-             'Sensor low battery', 'Sensor lost supervision'],
-            ['Zone bypassed', 'Force arm triggered by auto arm',
-             'Ready to arm', 'Ready to force arm', 'Valid PIN accepted',
-             'Chime on (sounding)', 'Error beep (triple beep)',
-             'Tone on (activation tone)'],
-            ['Entry 1', 'Open period', 'Alarm sent using phone number 1',
-             'Alarm sent using phone number 2',
-             'Alarm sent using phone number 3',
-             'Cancel report is in the stack',
-             'Keyswitch armed', 'Delay trip in progress (common zone)'],
-        ]
         partition = self._get_partition(frame.data[0] + 1)
         types = frame.data[1:5] + frame.data[6:8]
         partition.condition_flags = []
-        for byte, flags in enumerate(condition_flags):
+        for byte, flags in enumerate(model.Partition.CONDITION_FLAGS):
             for bit, name in enumerate(flags):
                 if types[byte] & (1 << bit):
                     partition.condition_flags.append(name)
@@ -259,47 +221,12 @@ class NXController(object):
             ext.obj.partition_status(partition)
 
     def process_msg_8(self, frame):
-        status_flags = [
-            ['Line seizure', 'Off hook', 'Initial handshake received',
-             'Download in progress', 'Dialer delay in progress',
-             'Using backup phone', 'Listen in active', 'Two way lockout'],
-            ['Ground fault', 'Phone fault', 'Fail to communicate',
-             'Fuse fault', 'Box tamper', 'Siren tamper/trouble', 'Low battery',
-             'AC fail'],
-            ['Expander box tamper', 'Expander AC failure',
-             'Expander low battery', 'Expander loss of supervision',
-             'Expander auxiliary output over current',
-             'Auxiliary communication channel failure',
-             'Expander bell fault', 'Reserved'],
-            ['6 digit PIN enabled', 'Programming token in use',
-             'PIN required for local download', 'Global pulsing buzzer',
-             'Global Siren on', 'Global steady siren',
-             'Bus device has line seized',
-             'Bus device has requested sniff mode'],
-            ['Dynamic battery test', 'AC power on', 'Low battery memory',
-             'Ground fault memory', 'Fire alarm verification being timed',
-             'Smoke power reset', '50 Hz line power detected',
-             'Timing a high voltage battery change'],
-            ['Communication since last autotest', 'Power up delay in progress',
-             'Walk test mode', 'Loss of system time', 'Enroll requested',
-             'Test fixture mode', 'Control shutdown mode',
-             'Timing a cancel window'],
-            ['reserved', 'reserved', 'reserved', 'reserved', 'reserved',
-             'reserved', 'reserved', 'Call back in progress'],
-            ['Phone line faulted', 'Voltage present interrupt active',
-             'House phone off hook', 'Phone line monitor enabled',
-             'Sniffing', 'Last read was off hook', 'Listen in requested',
-             'Listen in trigger'],
-            ['Valid partition 1', 'Valid partition 2', 'Valid partition 3',
-             'Valid partition 4', 'Valid partition 5', 'Valid partition 6',
-             'Valid partition 7', 'Valid partition 8'],
-        ]
-        errors = status_flags[1] + status_flags[2]
+        errors = model.System.STATUS_FLAGS[1] + model.System.STATUS_FLAGS[2]
         status = frame.data[1:10]
         self.system.panel_id = frame.data[0]
         orig_flags = self.system.status_flags
         self.system.status_flags = []
-        for byte, flags in enumerate(status_flags):
+        for byte, flags in enumerate(model.System.STATUS_FLAGS):
             for bit, name in enumerate(flags):
                 if status[byte] & (1 << bit):
                     self.system.status_flags.append(name)
--- nx584/model.py
+++ nx584/model.py
@@ -68,6 +68,22 @@ MSG_TYPES = [
 
 
 class Zone(object):
+    STATUS_FLAGS = [
+        'Faulted', 'Trouble', 'Bypass', 'Inhibit', 'Low battery',
+        'Loss of supervision', 'Reserved',]
+
+    TYPE_FLAGS = [
+        ['Fire', '24 hour', 'Key-switch', 'Follower',
+         'Entry / exit delay 1', 'Entry / exit delay 2',
+         'Interior', 'Local only'],
+        ['Keypad sounder', 'Yelping Siren', 'Steady Siren',
+         'Chime', 'Bypassable', 'Group bypassable', 'Force armable',
+         'Entry guard'],
+        ['Fast loop response', 'Double EOL tamper', 'Trouble',
+         'Cross zone', 'Dialer delay', 'Swinger shutdown',
+         'Restorable', 'Listen in'],
+    ]
+
     def __init__(self, number):
         self.number = number
         self.name = 'Unknown'
@@ -82,6 +98,30 @@ class Zone(object):
 
 
 class Partition(object):
+    CONDITION_FLAGS = [
+        ['Bypass code required', 'Fire trouble', 'Fire',
+         'Pulsing buzzer', 'TLM fault memory', 'reserved',
+         'Armed', 'Instant'],
+        ['Previous alarm', 'Siren on', 'Steady siren on',
+         'Alarm memory', 'Tamper', 'Cancel command entered',
+         'Code entered', 'Cancel pending'],
+        ['Reserved', 'Silent exit enabled', 'Entryguard (stay mode)',
+         'Chime mode on', 'Entry', 'Delay expiration warning',
+         'Exit 1', 'Exit 2'],
+        ['LED extinguish', 'Cross timing', 'Recent closing being timed',
+         'Reserved', 'Exit error triggered', 'Auto home inhibited',
+         'Sensor low battery', 'Sensor lost supervision'],
+        ['Zone bypassed', 'Force arm triggered by auto arm',
+         'Ready to arm', 'Ready to force arm', 'Valid PIN accepted',
+         'Chime on (sounding)', 'Error beep (triple beep)',
+         'Tone on (activation tone)'],
+        ['Entry 1', 'Open period', 'Alarm sent using phone number 1',
+         'Alarm sent using phone number 2',
+         'Alarm sent using phone number 3',
+         'Cancel report is in the stack',
+         'Keyswitch armed', 'Delay trip in progress (common zone)'],
+    ]
+
     def __init__(self, number):
         self.number = number
         self.condition_flags = []
@@ -93,6 +133,42 @@ class Partition(object):
 
 
 class System(object):
+    STATUS_FLAGS = [
+        ['Line seizure', 'Off hook', 'Initial handshake received',
+         'Download in progress', 'Dialer delay in progress',
+         'Using backup phone', 'Listen in active', 'Two way lockout'],
+        ['Ground fault', 'Phone fault', 'Fail to communicate',
+         'Fuse fault', 'Box tamper', 'Siren tamper/trouble', 'Low battery',
+         'AC fail'],
+        ['Expander box tamper', 'Expander AC failure',
+         'Expander low battery', 'Expander loss of supervision',
+         'Expander auxiliary output over current',
+         'Auxiliary communication channel failure',
+         'Expander bell fault', 'Reserved'],
+        ['6 digit PIN enabled', 'Programming token in use',
+         'PIN required for local download', 'Global pulsing buzzer',
+         'Global Siren on', 'Global steady siren',
+         'Bus device has line seized',
+         'Bus device has requested sniff mode'],
+        ['Dynamic battery test', 'AC power on', 'Low battery memory',
+         'Ground fault memory', 'Fire alarm verification being timed',
+         'Smoke power reset', '50 Hz line power detected',
+         'Timing a high voltage battery change'],
+        ['Communication since last autotest', 'Power up delay in progress',
+         'Walk test mode', 'Loss of system time', 'Enroll requested',
+         'Test fixture mode', 'Control shutdown mode',
+         'Timing a cancel window'],
+        ['reserved', 'reserved', 'reserved', 'reserved', 'reserved',
+         'reserved', 'reserved', 'Call back in progress'],
+        ['Phone line faulted', 'Voltage present interrupt active',
+         'House phone off hook', 'Phone line monitor enabled',
+         'Sniffing', 'Last read was off hook', 'Listen in requested',
+         'Listen in trigger'],
+        ['Valid partition 1', 'Valid partition 2', 'Valid partition 3',
+         'Valid partition 4', 'Valid partition 5', 'Valid partition 6',
+         'Valid partition 7', 'Valid partition 8'],
+    ]
+
     def __init__(self):
         self.panel_id = 0
         self.status_flags = []
