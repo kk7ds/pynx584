0cc2102c365f0363e25e3562f26ac3c3381d6393
Dan Smith <dsmith@danplanet.com>
2022-05-10 21:36:53 -0700

    Call this 0.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pynx584',
-      version='0.6',
+      version='0.7',
       description='NX584/NX8E Interface Library and Server',
       author='Dan Smith',
       author_email='dsmith+nx584@danplanet.com',
